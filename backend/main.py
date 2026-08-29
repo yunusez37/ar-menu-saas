0ea149eadf21e51dc7e38e84a560246180136d42
yunusez37 <zeytinliyunusemre@gmail.com>
2026-08-29 15:20:25 +0300

    CORS guvenlik kuralı ve frontend map hatasi duzeltildi

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI(title="QR AR Menü API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
